4dd06ce10573c6702bc07fd49169d58a0b6ba011
hg56th56gd6g <61111700+hg56th56gd6g@users.noreply.github.com>
2021-12-23 17:10:47 +0800

    Add files via upload

--- random.py
+++ random.py
@@ -1,36 +1,67 @@
 from time import time
 class RandomObject:
     def __init__(self):
-        self.index=1
-        self.state=[None]*624
-        self.state[0]=int(time())&4294967295
-        while self.index<624:
-            self.state[self.index]=(1812433253*(self.state[self.index-1]^(self.state[self.index-1]>>30))+self.index)&4294967295
-            self.index=self.index+1
+        index=1
+        state=[int(time())&4294967295]+[None]*623
+        while index<624:
+            state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
+            index=index+1
     def seed(self,a):
         assert type(a)==int,"seed must be a int"
-        self.index=1
-        self.state=[None]*624
-        self.state[0]=a&4294967295
-        while self.index<624:
-            self.state[self.index]=(1812433253*(self.state[self.index-1]^(self.state[self.index-1]>>30))+self.index)&4294967295
-            self.index=self.index+1
+        index=1
+        state=[a&4294967295]+[None]*623
+        while index<624:
+            state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
+            index=index+1
     def int32(self):
         a=0
-        if 624<=self.index:
+        if 624<=index:
             b=0
             while b<227:
-                a=(self.state[b]&2147483648)|(self.state[b+1]&2147483647)
-                self.state[b]=self.state[b+397]^(a>>1)^((a&1)*2567483615)
+                a=(state[b]&2147483648)|(state[b+1]&2147483647)
+                state[b]=state[b+397]^(a>>1)^((a&1)*2567483615)
                 b=b+1
             while b<623:
-                a=(self.state[b]&2147483648)|(self.state[b+1]&2147483647)
-                self.state[b]=self.state[b-227]^(a>>1)^((a&1)*2567483615)
+                a=(state[b]&2147483648)|(state[b+1]&2147483647)
+                state[b]=state[b-227]^(a>>1)^((a&1)*2567483615)
                 b=b+1
-            a=(self.state[623]&2147483648)|(self.state[0]&2147483647)
-            self.state[623]=self.state[396]^(a>>1)^((a&1)*2567483615)
-            self.index=1
-            return self.state[self.index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
-        self.index=self.index+1
-        return self.state[self.index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
-    random=lambda self:((self.int32()>>5)*67108864+(self.int32()>>6))/9007199254740992.0
+            a=(state[623]&2147483648)|(state[0]&2147483647)
+            state[623]=state[396]^(a>>1)^((a&1)*2567483615)
+            index=1
+            return state[1]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+        index=index+1
+        return state[index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+    random=lambda self:((int32()>>5)*67108864+(int32()>>6))/9007199254740992.0
+index=1
+state=[int(time())&4294967295]+[None]*623
+while index<624:
+    state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
+    index=index+1
+def seed(a):
+    assert type(a)==int,"seed must be a int"
+    global index,state
+    index=1
+    state=[a&4294967295]+[None]*623
+    while index<624:
+        state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
+        index=index+1
+def int32():
+    global index,state
+    a=0
+    if 624<=index:
+        b=0
+        while b<227:
+            a=(state[b]&2147483648)|(state[b+1]&2147483647)
+            state[b]=state[b+397]^(a>>1)^((a&1)*2567483615)
+            b=b+1
+        while b<623:
+            a=(state[b]&2147483648)|(state[b+1]&2147483647)
+            state[b]=state[b-227]^(a>>1)^((a&1)*2567483615)
+            b=b+1
+        a=(state[623]&2147483648)|(state[0]&2147483647)
+        state[623]=state[396]^(a>>1)^((a&1)*2567483615)
+        index=1
+        return state[1]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+    index=index+1
+    return state[index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+random=lambda:((int32()>>5)*67108864+(int32()>>6))/9007199254740992.0
