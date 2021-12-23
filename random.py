50d28df6ed9db3876bcd14628fd242a59de67508
hg56th56gd6g <61111700+hg56th56gd6g@users.noreply.github.com>
2021-12-23 16:28:51 +0800

    Add files via upload

--- random.py
+++ random.py
@@ -1,1 +1,36 @@
-exec("\x66\x72\x6f\x6d\x20\x5f\x72\x61\x6e\x64\x6f\x6d\x20\x69\x6d\x70\x6f\x72\x74\x20\x52\x61\x6e\x64\x6f\x6d\x0a\x61\x3d\x52\x61\x6e\x64\x6f\x6d\x28\x29\x0a\x66\x6f\x72\x20\x62\x20\x69\x6e\x20\x64\x69\x72\x28\x61\x29\x3a\x65\x78\x65\x63\x28\x22\x22\x2e\x6a\x6f\x69\x6e\x28\x28\x62\x2c\x22\x3d\x61\x2e\x22\x2c\x62\x29\x29\x29\x0a\x64\x65\x6c\x20\x62")
+from time import time
+class RandomObject:
+    def __init__(self):
+        self.index=1
+        self.state=[None]*624
+        self.state[0]=int(time())&4294967295
+        while self.index<624:
+            self.state[self.index]=(1812433253*(self.state[self.index-1]^(self.state[self.index-1]>>30))+self.index)&4294967295
+            self.index=self.index+1
+    def seed(self,a):
+        assert type(a)==int,"seed must be a int"
+        self.index=1
+        self.state=[None]*624
+        self.state[0]=a&4294967295
+        while self.index<624:
+            self.state[self.index]=(1812433253*(self.state[self.index-1]^(self.state[self.index-1]>>30))+self.index)&4294967295
+            self.index=self.index+1
+    def int32(self):
+        a=0
+        if 624<=self.index:
+            b=0
+            while b<227:
+                a=(self.state[b]&2147483648)|(self.state[b+1]&2147483647)
+                self.state[b]=self.state[b+397]^(a>>1)^((a&1)*2567483615)
+                b=b+1
+            while b<623:
+                a=(self.state[b]&2147483648)|(self.state[b+1]&2147483647)
+                self.state[b]=self.state[b-227]^(a>>1)^((a&1)*2567483615)
+                b=b+1
+            a=(self.state[623]&2147483648)|(self.state[0]&2147483647)
+            self.state[623]=self.state[396]^(a>>1)^((a&1)*2567483615)
+            self.index=1
+            return self.state[self.index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+        self.index=self.index+1
+        return self.state[self.index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+    random=lambda self:((self.int32()>>5)*67108864+(self.int32()>>6))/9007199254740992.0
