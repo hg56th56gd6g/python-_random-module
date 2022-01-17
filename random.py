fd0f5ac808c48a8f682c8ed700acae0ac54b230e
hg56th56gd6g <61111700+hg56th56gd6g@users.noreply.github.com>
2022-01-17 15:25:28 +0800

    Add files via upload

--- random.py
+++ random.py
@@ -1,67 +1,105 @@
 from time import time
 class RandomObject:
     def __init__(self):
-        index=1
-        state=[int(time())&4294967295]+[None]*623
-        while index<624:
-            state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
-            index=index+1
+        a=0
+        self.index=1
+        self.state=[int(time())&4294967295,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
+        while self.index<624:
+            self.state[self.index]=(1812433253*(self.state[a]^(self.state[a]>>30))+self.index)&4294967295
+            a=a+1
+            self.index=self.index+1
     def seed(self,a):
         assert type(a)==int,"seed must be a int"
+        b=0
         index=1
-        state=[a&4294967295]+[None]*623
-        while index<624:
-            state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
-            index=index+1
+        self.state=[a&4294967295,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
+        while self.index<624:
+            self.state[self.index]=(1812433253*(self.state[b]^(self.state[b]>>30))+self.index)&4294967295
+            b=b+1
+            self.index=self.index+1
     def int32(self):
         a=0
-        if 624<=index:
+        if 624<=self.index:
             b=0
+            c=1
+            d=397
             while b<227:
-                a=(state[b]&2147483648)|(state[b+1]&2147483647)
-                state[b]=state[b+397]^(a>>1)^((a&1)*2567483615)
+                a=(self.state[b]&2147483648)|(self.state[c]&2147483647)
+                self.state[b]=self.state[d]^(a>>1)^((a&1)*2567483615)
                 b=b+1
+                c=c+1
+                d=d+1
+            d=0
             while b<623:
-                a=(state[b]&2147483648)|(state[b+1]&2147483647)
-                state[b]=state[b-227]^(a>>1)^((a&1)*2567483615)
+                a=(self.state[b]&2147483648)|(self.state[c]&2147483647)
+                self.state[b]=self.state[d]^(a>>1)^((a&1)*2567483615)
                 b=b+1
-            a=(state[623]&2147483648)|(state[0]&2147483647)
-            state[623]=state[396]^(a>>1)^((a&1)*2567483615)
-            index=1
-            return state[1]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
-        index=index+1
-        return state[index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
-    random=lambda self:((int32()>>5)*67108864+(int32()>>6))/9007199254740992.0
+                c=c+1
+                d=d+1
+            a=(self.state[623]&2147483648)|(self.state[0]&2147483647)
+            self.state[623]=self.state[396]^(a>>1)^((a&1)*2567483615)
+            self.index=1
+            a=self.state[1]
+            a=a^(a>>11)
+            a=a^(a<<7)&2636928640
+            a=a^(a<<15)&4022730752
+            return a^(a>>18)
+        a=self.state[self.index]
+        self.index=self.index+1
+        a=a^(a>>11)
+        a=a^(a<<7)&2636928640
+        a=a^(a<<15)&4022730752
+        return a^(a>>18)
+    random=lambda self:((self.int32()>>5)*67108864+(self.int32()>>6))/9007199254740992.0
+state=[int(time())&4294967295,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
+b=0
 index=1
-state=[int(time())&4294967295]+[None]*623
 while index<624:
-    state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
+    state[index]=(1812433253*(state[b]^(state[b]>>30))+index)&4294967295
+    b=b+1
     index=index+1
 def seed(a):
     assert type(a)==int,"seed must be a int"
     global index,state
+    b=0
     index=1
-    state=[a&4294967295]+[None]*623
+    state=[a&4294967295,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
     while index<624:
-        state[index]=(1812433253*(state[index-1]^(state[index-1]>>30))+index)&4294967295
+        state[index]=(1812433253*(state[b]^(state[b]>>30))+index)&4294967295
+        b=b+1
         index=index+1
 def int32():
     global index,state
     a=0
     if 624<=index:
         b=0
+        c=1
+        d=397
         while b<227:
-            a=(state[b]&2147483648)|(state[b+1]&2147483647)
-            state[b]=state[b+397]^(a>>1)^((a&1)*2567483615)
+            a=(state[b]&2147483648)|(state[c]&2147483647)
+            state[b]=state[d]^(a>>1)^((a&1)*2567483615)
             b=b+1
+            c=c+1
+            d=d+1
+        d=0
         while b<623:
-            a=(state[b]&2147483648)|(state[b+1]&2147483647)
-            state[b]=state[b-227]^(a>>1)^((a&1)*2567483615)
+            a=(state[b]&2147483648)|(state[c]&2147483647)
+            state[b]=state[d]^(a>>1)^((a&1)*2567483615)
             b=b+1
+            c=c+1
+            d=d+1
         a=(state[623]&2147483648)|(state[0]&2147483647)
         state[623]=state[396]^(a>>1)^((a&1)*2567483615)
         index=1
-        return state[1]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+        a=state[1]
+        a=a^(a>>11)
+        a=a^(a<<7)&2636928640
+        a=a^(a<<15)&4022730752
+        return a^(a>>18)
+    a=state[index]
     index=index+1
-    return state[index]^(a>>11)^((a<<7)&2636928640)^((a<<15)&4022730752)^(a>>18)
+    a=a^(a>>11)
+    a=a^(a<<7)&2636928640
+    a=a^(a<<15)&4022730752
+    return a^(a>>18)
 random=lambda:((int32()>>5)*67108864+(int32()>>6))/9007199254740992.0
